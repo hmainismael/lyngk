4b0d4b66da3ffcb9441ac16d428b4a8603089662
hmainismael <hmain.ismael@hotmail.fr>
2017-09-29 15:52:11 +0200

    testStory2 v2

--- test/test.js
+++ test/test.js
@@ -9,7 +9,15 @@ LyngkTestCase.prototype.testStory1 = function () {
 };
 
 LyngkTestCase.prototype.testStory2 = function () {
-    var coordinates = new Lyngk.Coordinates();
-
-    assertTrue(coordinates.is_validTotalCoordinates());
+    var i, j, numberCoordinates = 0;
+    var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+    for (i = 1; i < 10; i++) {
+        for (j = 0; j < letters.length; j++) {
+            var coordinate = new Lyngk.Coordinates(letters[j], i);
+            if (coordinate.is_valid() == true) {
+                numberCoordinates++;
+            }
+        }
+    }
+    assertEquals(43,numberCoordinates);
 }
