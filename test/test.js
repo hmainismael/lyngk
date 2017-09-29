deb7d43668468fe0be1a6f6fad8b18730a236512
hmainismael <hmain.ismael@hotmail.fr>
2017-09-29 15:05:20 +0200

    testStory2

--- test/test.js
+++ test/test.js
@@ -3,7 +3,13 @@
 var LyngkTestCase = TestCase("LyngkTestCase");
 
 LyngkTestCase.prototype.testStory1 = function () {
-    var coordinates = new Lyngk.Coordinates('B', 4);
+    var coordinates = new Lyngk.Coordinates('A', 1);
 
     assertFalse(coordinates.is_valid());
 };
+
+LyngkTestCase.prototype.testStory2 = function () {
+    var coordinates = new Lyngk.Coordinates();
+
+    assertTrue(coordinates.is_validTotalCoordinates());
+}
