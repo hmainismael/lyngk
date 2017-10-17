336f777313e259c08ee268f26f5b57bcedc7e385
hmainismael <hmain.ismael@hotmail.fr>
2017-10-17 14:49:36 +0200

    solutionStory7

--- src/Intersection.js
+++ src/Intersection.js
@@ -3,4 +3,10 @@
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function (c, l) {
+
+    var etat = Lyngk.State.VACANT;
+
+    this.getState = function () {
+        return etat;
+    }
 };
